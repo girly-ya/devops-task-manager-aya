e16d37ed4626f02947b0d3858ba21acbb2b88a57
aya <ayayassine7me@gmail.com>
2026-02-25 14:10:35 +0000

    Update welcome message on feature branch

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ let tasks = [
 ];
 
 app.get('/', (req, res) => {
-  res.json({ message: "DevOps Task Manager API is running (heeere!)" });
+  res.json({ message: "Welcome from FEATURE branch" });
 });
 
 app.get('/tasks', (req, res) => {
